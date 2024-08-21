1dd205c4249da9f17ca9082cc41100f55b05a810
Aleksei Nushtaev <nushtaev2110@gmail.com>
2024-08-21 17:53:06 +0500

    Fix jsonify

--- json_maker.py
+++ json_maker.py
@@ -21,7 +21,7 @@ async def json_maker(dct):
         except Exception:
             print(name)
             await bot.send_message(CHANEL_ID, f"id {item['id']} - error\nhttp://37.143.15.242/api/v1/models")
-    for region in ['krasnodar', 'moscow', 'stavropol', 'surgut', 'volgograd', 'cheboksari']:
+    for region in ['krasnodar', 'moscow', 'stavropol', 'surgut', 'volgograd', 'chelyabinsk', 'cheboksari']:
         res_lst = []
         res = {}
         with open(f'csv/{region}.csv', 'r', encoding='utf-8') as csvfile:
