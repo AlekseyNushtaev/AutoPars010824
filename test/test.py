23e52dd5e802a5dcb93c36e82c05be454c998423
Aleksei Nushtaev <nushtaev2110@gmail.com>
2025-03-11 13:41:59 +0500

    Fix

--- test/test.py
+++ test/test.py
@@ -14,60 +14,60 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome
 
 
-async def astella_cars(dct_up, browser):
-    link = 'https://astella-cars.ru/new'
-    browser.get(link)
-    time.sleep(2)
-    html = browser.page_source
+def astella_cars(dct_up):
+    link = 'https://ufa.masmotors.ru/catalog/omoda'
+    response = requests.get(link)
+    html = response.text
     soup = bs4.BeautifulSoup(html, 'lxml')
-    brands = soup.find(attrs={"class": "brands-menu"}).find_all("a")
-    res = []
-    for brand_ in brands:
-        link_1 = brand_.get("href")
-        browser.get(link_1)
-        time.sleep(2)
-        html = browser.page_source
-        soup = bs4.BeautifulSoup(html, 'lxml')
-        try:
-            cards = soup.find(attrs={"class": "cars-block"}).find_all(attrs={"class": "cars-block__item-col cars-block__item-info"})
-        except AttributeError:
-            continue
-        for card in cards:
-            link = card.find(attrs={"class": "catalog-item__model link-black"}).get("href")
-            title = card.find(attrs={"class": "h3__brand"}).text.lower().strip()
-            brand = title.split()[0]
-            model = title.replace(brand, '').strip().replace(" ", "").replace("|", "i")
-            cost__ = card.find(attrs={"class": "catalog-item__price-current js_model-i-price"}).text
-            cost_ = ''
-            for y in cost__:
-                if y.isdigit():
-                    cost_ += y
-            cost = int(cost_)
-            name = brand + ', ' + model
-            try:
-                name = dct_up[name]
-            except KeyError:
-                await bot.send_message(CHANEL_ID, f'{name} {link}')
-            res.append([name, cost, link])
-    return res
+    print(soup.prettify())
+    # brands = soup.find(attrs={"class": "brands-menu"}).find_all("a")
+    # res = []
+    # for brand_ in brands:
+    #     link_1 = brand_.get("href")
+    #     browser.get(link_1)
+    #     time.sleep(2)
+    #     html = browser.page_source
+    #     soup = bs4.BeautifulSoup(html, 'lxml')
+    #     try:
+    #         cards = soup.find(attrs={"class": "cars-block"}).find_all(attrs={"class": "cars-block__item-col cars-block__item-info"})
+    #     except AttributeError:
+    #         continue
+    #     for card in cards:
+    #         link = card.find(attrs={"class": "catalog-item__model link-black"}).get("href")
+    #         title = card.find(attrs={"class": "h3__brand"}).text.lower().strip()
+    #         brand = title.split()[0]
+    #         model = title.replace(brand, '').strip().replace(" ", "").replace("|", "i")
+    #         cost__ = card.find(attrs={"class": "catalog-item__price-current js_model-i-price"}).text
+    #         cost_ = ''
+    #         for y in cost__:
+    #             if y.isdigit():
+    #                 cost_ += y
+    #         cost = int(cost_)
+    #         name = brand + ', ' + model
+    #         try:
+    #             name = dct_up[name]
+    #         except KeyError:
+    #             await bot.send_message(CHANEL_ID, f'{name} {link}')
+    #         res.append([name, cost, link])
+    # return res
 
 
 
 
-chrome_driver_path = ChromeDriverManager().install()
-browser_service = Service(executable_path=chrome_driver_path)
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument("--window-size=1200,600")
-
-options.add_argument('--disable-dev-shm-usage')
-browser = Chrome(service=browser_service, options=options)
-browser.maximize_window()
+# chrome_driver_path = ChromeDriverManager().install()
+# browser_service = Service(executable_path=chrome_driver_path)
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument("--window-size=1200,600")
+#
+# options.add_argument('--disable-dev-shm-usage')
+# browser = Chrome(service=browser_service, options=options)
+# browser.maximize_window()
 dct = {}
 with open('../autolist.txt', 'r', encoding='utf-8') as f:
     lst = f.readlines()
     for item in lst:
         dct[item.split('|')[0].strip()] = item.split('|')[1].strip()
-res = center_irtysh(dct, browser)
+res = astella_cars(dct)
 print(len(res))
